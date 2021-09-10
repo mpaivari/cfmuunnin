890360d5e8091dce34519aebeac4cb6d3496eff3
Päivärinta <Marko.Paivarinta@seamk.fi>
2021-09-10 15:43:19 +0300

    javascriptiä lisätty2

---
+++ index.js
@@ -0,0 +1,10 @@
+const input=document.querySelector('input');
+const fahrenheit=document.querySelector('#fahrenheit');
+const celsius=document.querySelector('#celsius');
+const button =document.querySelector('button');
+
+button.addEventListener('click', muunna);
+
+function muunna() {
+  alert('täällä ollaan');
+}
