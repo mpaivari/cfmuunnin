5e15b37a5417e6b20463c8c9447ee81bfb7ca59d
mpaivari <90454812+mpaivari@users.noreply.github.com>
2021-09-12 19:28:40 +0300

    Update index.js
    Fah - Celsius final

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function muunna() {
     console.log(fahAsteet);
     
     fahrenheit.textContent = fahAsteet;
-    celsius.textContent = (fahAsteet-32)/1.8;
+    celsius.textContent = Math.round((fahAsteet-32)/1.8*100))/100;
   }
   
 button.addEventListener('click', muunna);
