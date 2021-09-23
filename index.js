f91085946ce1452b38da07f8cc3dbaeddb0b35d6
mpaivari <90454812+mpaivari@users.noreply.github.com>
2021-09-23 15:38:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const input=document.querySelector('input');
-const radioMuunnos=document.querySelector('input[name="radioMuunnos"]');
+const muunnosCF=document.querySelector('input[name="muunnosCF"]');
+const muunnosFC=document.querySelector('input[name="muunnosFC"]');
 const fahrenheit=document.querySelector('#fahrenheit');
 const button =document.querySelector('button');
 const celsius=document.querySelector('#celsius');
@@ -8,19 +9,16 @@ function muunna() {
     fahAsteet = input.value;
     console.log(fahAsteet);
     
-    if (radioMuunnos.checked)  
+    if (muunnosFC.checked)  
     {
-      if (radioMuunnos.value = 'CF')  
-      {
-        fahrenheit.textContent = fahAsteet;
-        celsius.textContent = Math.round((fahAsteet*9/5)+32);    
-      }
-      if (radioMuunnos.value = 'FC') 
-      {
         fahrenheit.textContent = fahAsteet;
         celsius.textContent = Math.round((fahAsteet-32)/1.8*100)/100;
-      }
-    }     
+    }   
+    if (muunnosCF.checked)  
+    {  
+        fahrenheit.textContent = fahAsteet;
+        celsius.textContent = Math.round((fahAsteet*9/5)+32);  
+    }
   }
   
 button.addEventListener('click', muunna);
