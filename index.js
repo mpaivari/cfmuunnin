85c1ba16cc3bb5f6cbacd2c06dd830fa93032e35
mpaivari <90454812+mpaivari@users.noreply.github.com>
2021-09-23 14:27:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const input=document.querySelector('input');
+const select=document.querySelector('select');
 const fahrenheit=document.querySelector('#fahrenheit');
 const button =document.querySelector('button');
 const celsius=document.querySelector('#celsius');
@@ -8,8 +9,16 @@ function muunna() {
      
     console.log(fahAsteet);
     
-    fahrenheit.textContent = fahAsteet;
-    celsius.textContent = Math.round((fahAsteet-32)/1.8*100)/100;
+    if select.value = 'FC' then 
+    {
+      fahrenheit.textContent = fahAsteet;
+      celsius.textContent = Math.round((fahAsteet-32)/1.8*100)/100;
+    }
+    else
+    {
+      fahrenheit.textContent = fahAsteet;
+      celsius.textContent = Math.round((fahAsteet*9/5)+32);    
+    }
   }
   
 button.addEventListener('click', muunna);
